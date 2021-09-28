1cac6435a6074dc46982034e3931f7d5fe5bbbd1
Murillo Bilches Navarro <murillo.navarro@pitang.com>
2021-09-28 17:03:55 -0300

    Apresentando os posters na pagina

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
 
 export default () => {
 
@@ -16,11 +17,12 @@ export default () => {
   }, []);
 
   return (
-  <div className="page">
-    Header
-    Destaque
-    As listas
-    Rodapé
-  </div>
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
   );
 }
