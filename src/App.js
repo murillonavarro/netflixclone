f20dcca4604e1fece4618e856759515361f4e53f
Murillo Bilches Navarro <murillo.navarro@pitang.com>
2021-09-28 16:34:39 -0300

    Iniciando a página

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
 
   useEffect(()=>{
     const loadAll = async () => {
       // Pegando a lista TOTAL
       let list = await Tmdb.getHomeList();
+      setMovieList(list);
     }
 
     loadAll();
   }, []);
 
   return (
-  <div>
-    Olá Mundo!
+  <div className="page">
+    Header
+    Destaque
+    As listas
+    Rodapé
   </div>
   );
 }
